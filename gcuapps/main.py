b547e752a66172bea3c1ed4764d4efde1c5cd3f7
thskumar <thshantakumar@gmail.com>
2024-12-29 23:03:22 +0530

    Update main.py

--- gcuapps/main.py
+++ gcuapps/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import scorecard, attendance, home
+import scorecard, attendance, home, gcu_program_result
 
 st.set_page_config(
     page_title="GCU Apps",
